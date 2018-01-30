047cbb685b29270ee97d472b804acc01f160cf2f
Glitch (php-contacts-anjana) <none>
2018-01-30 17:15:20 +0000

    😱😾 Checkpoint README.md:243787/542 MVC.js:243787/320

--- MVC.js
+++ MVC.js
@@ -1,8 +1,18 @@
 const m = {} //MODEL (holds state variables and constants)
 const v = {} //VIEW (holds viewable input-output components)
 const c = {} //CONTROLLER (holds all the function properties = methods)
+//=============| STATE VARIABLES |================//
+m.contacts = [
+  ["Lastname","Firstname","Email","Phone"],
+  ["Lastname 2","Firstname 2","Email 2","Phone 2"]
+]
+m.passwordIsValid = false
+
 //=================| INITIALIZE |===================//
 c.initialize = function(){
   
 }
 //==============| END OF INITIALIZE |=============//
+c.getPassword = function(){}
+c.downloadContacts = function(){}
+c.showContacts = function(){}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# PHP Proof of Concept
-https://glitch.com/edit/#!/php-base# php-contacts
+### An evolving project to make a web-based contact list of friends and family to call or email
+
+It should:
+* ...have a place to store contacts
+* ...store contacts on the glitch server
+* ...have a way to show contacts
+* ...have a way to add contacts
+* ...have a way to delete contacts
+* ...have a way to update contacts
+* ...have a way to search contacts
+* ...be password protected
