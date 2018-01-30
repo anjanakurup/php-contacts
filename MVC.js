94ee7b2576ee3e7ac4a31bb104a63aecdbb50e09
Glitch (php-contacts-anjana) <none>
2018-01-30 16:47:49 +0000

    basic template

--- MVC.js
+++ MVC.js
@@ -1,4 +1,8 @@
 const m = {} //MODEL (holds state variables and constants)
-const v = {} //VIEW (h
-const c = {} //CONTROLLER
-//=============| INITIALIZE |===============//
+const v = {} //VIEW (holds viewable input-output components)
+const c = {} //CONTROLLER (holds all the function properties = methods)
+//=================| INITIALIZE |===================//
+c.initialize = function(){
+  
+}
+//==============| END OF INITIALIZE |=============//
