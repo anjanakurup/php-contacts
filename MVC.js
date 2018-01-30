72980f449fdacdc6f357c5b695f879a89cfece31
Glitch (php-contacts-anjana) <none>
2018-01-30 17:47:19 +0000

    🐌🚶 Checkpoint MVC.js:243787/353

--- MVC.js
+++ MVC.js
@@ -13,7 +13,7 @@ c.initialize = function(){
   
 }
 //==============| END OF INITIALIZE |=============//
-c.getPassword = function(guessedPassword){
+c.checkPassword = function(guessedPassword){
   //The actual password is found in the checkPassword.php file
   //make a postman
   const postman = new XMLHttpRequest()
@@ -21,18 +21,37 @@ c.getPassword = function(guessedPassword){
   postman.send(guessedPassword)
   //wait for a response with a load event from server
   postman.addEventListener('load', function(){
-    alert(postman.response)
-})
+    if(postman.status === 200){
+      const access = postman.responseText
+      m.passwordIsValid = (access === 'ok') ? true : false
+    }
+    else{
+      m.passwordIsValid = false
+      console.log('Trouble at the server')
+    }
+      
+  })
+  postman.addEventListener('error', function(){
+    
+  })
+}  
+
 c.downloadContacts = function(){}
+
 c.addContact = function(){}
+
 c.saveContact = function(){}
+
 c.deleteContact = function(){}
+
 c.addAndSave = function(){
   c.addContact()
   c.saveContact()
 }
+
 c.deleteAndSave = function(){
   c.deleteContact()
   c.saveCOntact()
 }
+
 c.showContacts = function(){}
